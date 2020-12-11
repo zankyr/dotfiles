79cce241692a79e2f47f93674517cab9e8f18019
rik <p3mal88@gmail.com>
2020-12-11 14:09:20 +0100

    Add update function for bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,10 +2,25 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin main;
+#git pull origin main;
+
+function usage {
+	cat <<HELP_USAGE
+
+Usage: $0 OPTIONS
+
+A script to quickly configure your MacOs Envoriment.
+
+Options:
+	-i, --install Install the scripts in the system.
+	-u, --update  Update the scripts from the system to the current folder.
+	-h, --help    Show usage
+
+HELP_USAGE
+}
 
 # Copy all the files and folders to the home dir, then compile .bash_profile
-function doIt() {
+function install {
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude "bootstrap.sh" \
@@ -14,11 +29,35 @@ function doIt() {
 	source ~/.bash_profile;
 }
 
+# Sync the expected files from the home dir to the current folder
+function update {
+	SRC_FOLDER=$HOME;
 
-read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-echo "";
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	doIt;
-fi;
+	rsync --exclude "$SRC_FOLDER/.git/" \
+		--exclude "$SRC_FOLDER/.DS_Store" \
+		--exclude "$SRC_FOLDER/bootstrap.sh" \
+		--exclude "$SRC_FOLDER/README.md" \
+		-avhr --files-from=from-file.txt --no-perms $SRC_FOLDER .;
+}
 
-unset doIt;
+case $1 in 
+	"-i" | "--install" )
+	echo "INSTALL";
+	;;
+	"-u" | "--update" )
+	update
+	;;
+	* )
+	usage
+	;;
+esac
+
+
+
+#read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+#echo "";
+#if [[ $REPLY =~ ^[Yy]$ ]]; then
+	#doIt;
+#fi;
+
+unset install;
